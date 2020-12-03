29c6bdaca6cfa8ba044ceeac14470c267c909860
Theelo <elvinas.elo@gmail.com>
2020-12-03 17:49:34 +0200

    Updates

---
+++ precache-manifest.d3bb453d2f86eb556f70a9e50378eb12.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a555f73f6b9279b05c7899970a4543e",
+    "url": "/soundmakr/index.html"
+  },
+  {
+    "revision": "97a272c9bbf801c80f23",
+    "url": "/soundmakr/static/css/main.8664d01a.chunk.css"
+  },
+  {
+    "revision": "18fe7087b0fb45a498b4",
+    "url": "/soundmakr/static/js/2.fb83bee6.chunk.js"
+  },
+  {
+    "revision": "73152a6b2807fa9811ff3afd65903070",
+    "url": "/soundmakr/static/js/2.fb83bee6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97a272c9bbf801c80f23",
+    "url": "/soundmakr/static/js/main.6b87dabd.chunk.js"
+  },
+  {
+    "revision": "dddc30b188e475f9ccdc",
+    "url": "/soundmakr/static/js/runtime-main.c5b79f65.js"
+  },
+  {
+    "revision": "3b5635ac5d39c28200bf3993d90d09c7",
+    "url": "/soundmakr/static/media/Raleway-VariableFont_wght.3b5635ac.ttf"
+  },
+  {
+    "revision": "e07df86cef2e721115583d61d1fb68a6",
+    "url": "/soundmakr/static/media/Roboto-Bold.e07df86c.ttf"
+  },
+  {
+    "revision": "58aef543c97bbaf6a9896e8484456d98",
+    "url": "/soundmakr/static/media/Roboto-Medium.58aef543.ttf"
+  },
+  {
+    "revision": "11eabca2251325cfc5589c9c6fb57b46",
+    "url": "/soundmakr/static/media/Roboto-Regular.11eabca2.ttf"
+  }
+]);
